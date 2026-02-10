dd91e18f8ba294bfb7e73ca9987ff6f791c20f47
moamen memy <memmoamen82@gmail.com>
2026-02-10 14:52:14 +0200

    ششششششش

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HeroComponent } from "./features/hero/hero.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/hero/hero.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductComponent } from './pages/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', loadComponent:()=> import('./features/hero/hero.component')  .then(c => c.HeroComponent) },        // الصفحة الافتراضية
+  { path: 'products', loadComponent:()=> import('./pages/product/product.component')  .then(c => c.ProductComponent) },
+  {path: 'contact', loadComponent:()=> import('./pages/contact/contact.component')  .then(c => c.ContactComponent) },
+  {path:'tray', loadComponent:()=> import('./pages/tray/tray.component')  .then(c => c.TrayComponent) },
+  {path:'children', loadComponent:()=> import('./pages/children/children.component')  .then(c => c.ChildrenComponent) },
+  {path:'napkins', loadComponent:()=> import('./pages/napkins/napkins.component')  .then(c => c.NapkinsComponent) },
+  {path:'concrete', loadComponent:()=> import('./pages/concrete/concrete.component')  .then(c => c.ConcreteComponent) },
+  {path:'**', redirectTo: ''}
+];
